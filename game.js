5ee3b6544bc24ec0804f2c2bdd47c7d493f659af
umitgenis <umitgenis@gmail.com>
2022-10-25 00:02:15 +0300

    Refactoring game.js

--- game.js
+++ game.js
@@ -1,18 +1,13 @@
 /* 
 Plan;
-
 - Rast gele bir sayı oluştur
 - OluŞan sayıya göre resimleri değiştir.
-- Resimleri bg atayabilmek için bir array oluşturalım.
+- Resimleri bg atayabilmek için bir array oluştur.
     bgImg = ["url(/makas.svg)","url(/kagit.svg)","url(/tas.svg)"];
-
-! extras yapılcakcakar
-    
+- Extra yapılcaklar    
 */
 
-// var bgImg = ["url(./assets/makas.svg)",
-//              "url(./assets/kagit.svg)",
-//              "url(./assets/tas.svg)"];
+
 
 var bgImg = ["makas.svg",
              "kagit.svg",
@@ -24,27 +19,15 @@ var player2Img = document.getElementById("player2Img").style;
 function randomImg() {
     var random = Math.round(Math.random()*2);
     return random;
-}
-
-//   function randomImg2() {
-//       var random2 = Math.round(Math.random()*2);
-//       return random2;
-//   }          
+}       
 
 var player1ImgState;
-//   var player2ImgState;
+var player2ImgState;
 
 function givePlayerImg() {
-
-//   randomImg();
-//   randomImg2();              
-    
-    // player1ImgState = player1Img.backgroundImage = bgImg[randomImg()];
-    // player2ImgState = player2Img.backgroundImage = bgImg[randomImg()];
     
     player1ImgState = player1Img.backgroundImage = "url(./assets/" + bgImg[randomImg()] + ")";
-    player2ImgState = player2Img.backgroundImage = "url(./assets/" + bgImg[randomImg()] + ")";  
-    
+    player2ImgState = player2Img.backgroundImage = "url(./assets/" + bgImg[randomImg()] + ")";      
 }
 
 function state() {
@@ -65,48 +48,34 @@ function state() {
     if(player1ImgState == mks && player2ImgState == kgt ){
         p1.innerHTML = player1Skor + 1;
         player1ImgBgColor.backgroundColor = player1SkorBgColor.backgroundColor = "green";
-        player2ImgBgColor.backgroundColor = player2SkorBgColor.backgroundColor = "red";
-        // player1SkorBgColor.backgroundColor = "green";
-        // player2SkorBgColor.backgroundColor = "red";  
+        player2ImgBgColor.backgroundColor = player2SkorBgColor.backgroundColor = "red";         
     }
     else if(player1ImgState == mks && player2ImgState == ts ){
         p2.innerHTML = player2Skor + 1;
         player1ImgBgColor.backgroundColor = player1SkorBgColor.backgroundColor = "red";
         player2ImgBgColor.backgroundColor = player2SkorBgColor.backgroundColor = "green";
-        // player1SkorBgColor.backgroundColor = "red";
-        // player2SkorBgColor.backgroundColor = "green";
+        
     }
     else if(player1ImgState == ts && player2ImgState == kgt ){
         p2.innerHTML = player2Skor + 1;
         player1ImgBgColor.backgroundColor = player1SkorBgColor.backgroundColor = "red";
-        player2ImgBgColor.backgroundColor = player2SkorBgColor.backgroundColor = "green";
-        // player1SkorBgColor.backgroundColor = "red";
-        // player2SkorBgColor.backgroundColor = "green";
+        player2ImgBgColor.backgroundColor = player2SkorBgColor.backgroundColor = "green";        
     }
     else if(player1ImgState == ts && player2ImgState == mks ){
         p1.innerHTML = player1Skor + 1;
         player1ImgBgColor.backgroundColor = player1SkorBgColor.backgroundColor = "green";
         player2ImgBgColor.backgroundColor = player2SkorBgColor.backgroundColor = "red";
-        // player1SkorBgColor.backgroundColor = "green";
-        // player2SkorBgColor.backgroundColor = "red";
     }
     else if(player1ImgState == kgt && player2ImgState == ts ){
         p1.innerHTML = player1Skor + 1;
         player1ImgBgColor.backgroundColor = player1SkorBgColor.backgroundColor = "green";
         player2ImgBgColor.backgroundColor = player2SkorBgColor.backgroundColor = "red";
-        // player1SkorBgColor.backgroundColor = "green";
-        // player2SkorBgColor.backgroundColor = "red";
     }
     else if(player1ImgState == kgt && player2ImgState == mks ){
         p2.innerHTML = player2Skor + 1;
         player1ImgBgColor.backgroundColor = player1SkorBgColor.backgroundColor = "red";
         player2ImgBgColor.backgroundColor = player2SkorBgColor.backgroundColor = "green";
-        // player1SkorBgColor.backgroundColor = "red";
-        // player2SkorBgColor.backgroundColor = "green";
-    }              
-
-    // console.log( typeof player1Skor );
-    // console.log( typeof player2Skor  );    
+    }            
 }
 
 function run() {
@@ -116,5 +85,3 @@ function run() {
 
 document.getElementById("gameStart-btn").addEventListener("click", run); 
         
-// console.log( player1ImgState );
-// console.log( player2ImgState);
